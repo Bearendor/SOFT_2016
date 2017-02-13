9cc9e1550ea2005e30a17b09f37b4fe2fa8ac43a
Bearendor <beramarko@yahoo.com>
2017-02-13 18:51:42 +0100

    uklonjene tackice sa ekrana i slika prebacena u gray.

--- soft2016/project.py
+++ soft2016/project.py
@@ -1,16 +1,28 @@
+import numpy as np
 import cv2
 
-path = "D:\soft\level-2\\video-0.avi"
+#pathSoko = "D:\soft\level-2\\video-0.avi"
 
-cap = cv2.VideoCapture(path)
+pathBera = "C:\\Users\\Marko Bera\\Desktop\\SOFT\\level-2\\video-0.avi"
+
+cap = cv2.VideoCapture(pathBera)
 while(cap.isOpened()):
     ret, frame = cap.read()
 
-    #cv2.imshow('res',res)
-    #cv2.imshow('mask',mask)
-    #cv2.imshow('gray',gray)
-    #cv2.imshow('opening',opening)
-    cv2.imshow('frame', frame)
+
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    lower_green = np.array([20, 20, 20])
+    upper_green = np.array([100, 255, 255])
+
+    mask = cv2.inRange(hsv, lower_green, upper_green)
+    res = cv2.bitwise_and(frame, frame, mask = cv2.bitwise_not(mask))
+
+    gray = cv2.cvtColor(res, cv2.COLOR_BGR2GRAY)
+    kernel = np.ones((2,2),np.uint8)
+    opening = cv2.morphologyEx(gray, cv2.MORPH_OPEN, kernel)
+
+    cv2.imshow('opening',opening)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
